90caff03f6c72d6c3484803f0029f2198e5f1e32
qr-codigo <majox1840@gmail.com>
2026-03-25 13:02:25 -0600

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -12,25 +12,32 @@ document.querySelector("#submit").addEventListener("click", e => {
   console.log(cliente.toUpperCase());
   console.log(pedido.toUpperCase());
 
+  const fecha = new Date();
+const dia = fecha.getDate();
+const mes = fecha.getMonth() + 1; // Los meses van de 0-11, se suma 1 [1]
+const anio = fecha.getFullYear();
+console.log(`${dia}/${mes}/${anio}`); 
+
+
+
   resp.classList.remove("fail");
   resp.classList.remove("send");
 
   const url = `https://api.whatsapp.com/send?phone=${telefono}&text=
-		📲*PEDIDO NUEVO*%0A
-		😋*RAÍZ DULCE*%0A
+		📲 *PEDIDO NUEVO*%0A
+		📅 *${dia}/${mes}/${anio}*%0A
  ------------------------------------%0A
-		👤*Nombre*%0A
+		👤 *Nombre*%0A
 		- ${cliente}%0A
   ------------------------------------%0A
-		⏰*Hora de recoger pedido*%0A
+		⏰ *Hora de recoger pedido*%0A
 		- ${hora}%0A
   ------------------------------------%0A
-		💳*Método de pago*%0A
+		💳 *Método de pago*%0A
 		- ${empleado}%0A
   ------------------------------------%0A
-		✅*Pedido*%0A
+		✅ *Pedido*%0A
 		- ${pedido}`;
-
 
 
   if (cliente === "" || hora === "" || pedido === "") {
@@ -42,9 +49,12 @@ document.querySelector("#submit").addEventListener("click", e => {
 });
    
     return false;
-  }  
+  }
 
-    if (hora >= "07:50" && hora <= "20:15") {
+
+  if (fecha.getDay() === 0) {
+  console.log("¡Es domingo!");
+   if (hora >= "10:10" && hora <= "13:45") {
         console.log("Dentro del horario laboral");
         
         resp.classList.remove("fail");
@@ -65,6 +75,34 @@ Swal.fire({
 
         return false;
     }
+
+} else {
+  console.log("No es domingo.");
+   if (hora >= "07:50" && hora <= "20:15") {
+        console.log("Dentro del horario laboral");
+        
+        resp.classList.remove("fail");
+  resp.classList.add("send");
+  window.open(url);
+Swal.fire({
+  title: "¡Pedido realizado con éxito!",
+  icon: "success",
+  draggable: true
+});
+    } else {
+      Swal.fire({
+  icon: "error",
+  title: "Lo sentimos",
+  text: "¡Pedido fuera de horario laboral!",
+  footer: ""
+});
+
+        return false;
+    }
+}
+  
+
+   
 
 
   
